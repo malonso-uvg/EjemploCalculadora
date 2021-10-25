99553fe7ed98cb5d04a658db0ced8afd60c44364
Brandon <sicayb@gmail,com>
2021-10-25 16:56:21 -0600

    Saludo Brandon

--- src/org/calculadora/ui/Saludos.java
+++ src/org/calculadora/ui/Saludos.java
@@ -20,4 +20,8 @@ public class Saludos {
 	public void SaludoJose() {
 		System.out.println("Hola a todos soy José");
 	}
+        
+        public void SaludoBrandon() {
+            System.out.println("Hola a todos soy Brandon");
+        }
 }
